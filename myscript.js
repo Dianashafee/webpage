61332c6da4897a2e2fdf5a7a9ab78a8a6c1aee25
Dianashafee <diana.shafee1997@hotmail.com>
2020-08-26 12:52:19 +0300

    updating webpage

--- myscript.js
+++ myscript.js
@@ -15,4 +15,23 @@ if (age > 18) {
 document.write('<h3>'+greeting+'</h3>');
 
 var name = prompt('what is your name?');
-var feeling = prompt('are you excited to shop with us?');
+var feeling = prompt('are you excited to shop with us? ' + name);
+
+function showorder(name){
+  //input 
+  var userInput = prompt('Do you to do Hijab shopping or regular shopping? '+name);
+
+  //process 
+  if (userInput == "hijab shopping"){
+    document.write('<img src="https://64.media.tumblr.com/06a95c8df592c2b5df26e7068f44be02/tumblr_p8gnmru8T21v7bwzjo4_500.jpg" alt="img2">');
+   } else if (userInput == "regular shopping"){
+     document.write('<img src="./white.jpg" alt="img3">');
+  } else {
+  var error = alert('choose right next time');
+}
+}
+  showorder(name ); 
+
+  //output
+
+ 
